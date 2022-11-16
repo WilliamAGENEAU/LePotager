e882001acf70b27c73542c79c56ed5e66089f12f
WilliamAGENEAU <wageneau.ir2024@esaip.org>
2022-11-16 14:58:27 +0100

    favicon

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 
 <head>
     <title>D4G grp 14</title>
+	<link rel="icon" href="images\favicon.ico" type="image/x-icon">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="D4G">
-    <link rel="icon"  href="images" type="image/x-icon">
     <link rel="stylesheet" href="css/style.css">
     <script src="main.js"></script>
 </head>
