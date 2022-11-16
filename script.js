7d4611e95e919758022446501eaad98644e5ab2c
VeaurryceBinhnayt <94529135+VeaurryceBinhnayt@users.noreply.github.com>
2022-11-16 17:58:40 +0100

    up json

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-fetch("Fichier_csv.json")
+fetch("utils/data.geojson")
                 .then(response => response.json())
                 .then(data =>{
-                    
-                        
-                        for(var r=0;r<data.FR.length;r++)
+
+                    for(var r=0;r<data.features.length;r++)
                         {
                         var x=document.getElementById('data-output').insertRow(r);
                         /*for(var c=0;c<14;c++)  
                             {
-                                */
+                               */ 
                             var y=  x.insertCell(0);
-                            y.innerHTML= data.FR[r].Formation_Continue_initiale
+                            y.innerHTML= data.features[r].properties["Formation_Continue_initiale"]
                             var y=  x.insertCell(1);
-                            y.innerHTML= data.FR[r].Vous_êtes_une_structure
+                            y.innerHTML= data.features[r].properties["Vous_êtes_une_structure"]
                             var y=  x.insertCell(2);
-                            y.innerHTML= data.FR[r].Courte_description_du_programme_Contenu
+                            y.innerHTML= data.features[r].properties["Courte_description_du_programme_Contenu"]
                             var y=  x.insertCell(3);
-                            y.innerHTML= data.FR[r].Localisation_de_la_structure
+                            y.innerHTML= data.features[r].properties["Localisation_de_la_structure"]
                             //}
                         }
                         
