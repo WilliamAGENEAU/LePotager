963f9dba57fcd7d6194e68f1e893f1f8b01fe6c0
EliottNUEZ <enuez.ir2024@esaip.org>
2022-11-17 19:52:10 +0100

    .

--- index.php
+++ index.php
@@ -1451,6 +1451,7 @@
  
 ];
 </script>
+<script src="https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js"></script>
 </head>
 <nav role='navigation'>
   
--- main.js
+++ main.js
@@ -5,34 +5,45 @@ function init() {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(map);
+
     
+    L.control.geocoder().addTo(map);
+    L.control.scale({ position: 'bottomleft' }).addTo(map);
+
     Object.entries(json).forEach(objet => {
         var marker = L.marker([objet[1].Longitude,objet[1].Latitude]).addTo(map);
         marker.bindPopup(objet[1].NOM);
     })
 }
 
+function recherche() {
+	var input, filter, found, table, tr, td, i, j;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < td.length; j++) {
+            if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                found = true;
+            }
+        }
+        if (found) {
+            tr[i].style.display = "";
+            found = false;
+        } else {
+            tr[i].style.display = "none";
+        }
+    }
+    /*
+    var map = document.getElementById("map");
+    console.log(map);*/
+}
+
 var panier = [];
 
 var myjson = [];
-
-fetch("utils/Fichier_csv.json")
-                .then(response => response.json())
-                .then(data =>{
-                    let text = "<table cellpadding='0' cellspacing='0' border='0'>"
-                    text += "<tbody>";
-                    for(var r=0;r<data.length;r++)
-                        {
-                            text += "<tr><td>" + data[r].Formation_Continue_initiale + "</td><td>" 
-                            + data[r].Type_organisme + "</td><td>"+ data[r].NOM + "</td><td>"+ data[r].Intitule_de_la_formation + 
-                            "</td><td>"+ data[r].Localisation_de_la_structure + "</td><td>"+ data[r].Duree_de_la_formation +
-                            "</td><td>"+ data[r].Modalites + "</td><td><button onclick='addPanier("+r+")'>Ajouter au panier</button></td></tr>"; 
-                            myjson.push(data[r]);
-                        }
-                        text += "</tbody>"
-                        text += "</table>"
-                        document.getElementById("data-output").innerHTML = text;
-                })
 
 function printDiv() {
     var divContents = document.getElementById("panier").innerHTML;
@@ -53,20 +64,26 @@ function printDiv() {
 function addPanier(r){
     if(panier.length<3){
         panier.push(myjson[r]);
-    }else{
-        alert('Votre panier est déja plein');
     }
     console.log(panier);
 }
 
-function afficherPanier(){
-    let text = "";
-    for(var i = 0; i<panier.length; i++){
-        text += "<li>"+ panier[i] +"</li>";
-    }
-    document.getElementById("panier").innerHTML = text;
-}
 
-function supprPanier(r){
 
-}
+fetch("utils/data.geojson")
+                .then(response => response.json())
+                .then(data =>{
+                    let text = "<table cellpadding='0' cellspacing='0' border='0'>"
+                    text += "<tbody>";
+                    for(var r=0;r<data.features.length;r++)
+                        {
+                            text += "<tr><td>" + data.features[r].properties["Formation_Continue_initiale"] + "</td><td>" 
+                            + data.features[r].properties["Type_organisme"] + "</td><td>"+ data.features[r].properties["NOM_de_l'organisme"] + "</td><td>"+ data.features[r].properties["Intitule_de_la_formation"] + 
+                            "</td><td>"+ data.features[r].properties["Localisation_de_la_structure"] + "</td><td>"+ data.features[r].properties["Duree_de_la_formation"] +
+                            "</td><td>"+ data.features[r].properties["Modalites"] + "</td><td><button onclick='addPanier("+r+")'>Ajouter au panier</button></td></tr>"; 
+                            myjson.push(data.features[r].properties);
+                        }
+                        text += "</tbody>"
+                        text += "</table>"
+                        document.getElementById("data-output").innerHTML = text;
+                })
