e781884f9de99e1fb13975dc467a100c4bcff084
VeaurryceBinhnayt <94529135+VeaurryceBinhnayt@users.noreply.github.com>
2022-11-17 16:50:24 +0100

    up

--- main.js
+++ main.js
@@ -11,3 +11,26 @@ function init() {
         marker.bindPopup(objet[1].NOM);
     })
 }
+
+function printDiv() {
+    var divContents = document.getElementById("panier").innerHTML;
+    var a = window.open('', '', 'height=800, width=800');
+    a.document.write('<html>');
+    a.document.write(
+        '<head><link rel="stylesheet" href="./../css/style.css" type="text/css"><link rel="stylesheet" href="./../utils/lib/bootstrap.min.css" /></head>'
+    );
+    a.document.write('<body>');
+    a.document.write('<h1>Devis</h1>');
+    a.document.write('<p>Liste des formations sélectionnées :</p>');
+    a.document.write(divContents);
+    a.document.write('</body></html>');
+    a.document.close();
+    a.print();
+}
+
+function addPanier(r){
+    if(panier.length<3){
+        panier.push(json[r]);
+    }
+    console.log(panier);
+}
