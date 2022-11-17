f0434604cbcb487b68d4f6320aa5dcbf8a6648cf
EliottNUEZ <enuez.ir2024@esaip.org>
2022-11-17 19:52:29 +0100

    Revert "tableau"
    This reverts commit 8a5fe8d806be8ea9a2de439cfdca75b1f3ece5fa.

--- index.php
+++ index.php
@@ -1487,12 +1487,12 @@
                         <tr>
                             <th> Formation Continue / initiale : </th>
                             <th> Type d'organisme : </th>
-                            <th> Nom d'organisme : </th>
-                            <th> Intitulé de la formation : </th>
-                            <th> Localisation de la structure (ville) : </th>
-                            <th> Durée de la formation / intervention (en j) : </th>
-                            <th> Modalités d’accès à la formation (présentiel / distanciel / hybride) : </th>
-                            <th></th>
+                            
+                                <th> Intitulé de la formation : </th>
+                                <th> Localisation de la structure (ville) : </th>
+                                <th> Durée de la formation / intervention (en j) : </th>
+                                <th> Modalités d’accès à la formation (présentiel / distanciel / hybride) : </th>
+                                <th></th>
                                 
                             </tr>
                         </thead>
