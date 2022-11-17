04bc529855b444b758495dd3bfd16f3db209f6ea
WilliamAGENEAU <wageneau.ir2024@esaip.org>
2022-11-17 17:55:00 +0100

    panier

--- index.php
+++ index.php
@@ -1453,26 +1453,8 @@
 </script>
 </head>
 <nav role='navigation'>
-  <div id="menuToggle">
-    <input type="checkbox" onclick='afficherPanier()'/>
-    <span></span>
-    <span></span>
-    <span></span>
-    <ul id="menu">
+  
 
-	  <div id="outer">
-  		<div class="inner">
-		  <button type="submit" onclick="printDiv()"><i ></i> Print</button>
-	  </div>
-	  <div class="inner">
-	  <form action="https://formspree.io/f/mwkzywdl" method="POST"><br>
-                <input type="email" name="_replyto" placeholder="Email"><br>
-                <button type="submit">Devis</button>
-            </form>
-	  </div>
-	  </div>
-    </ul>
-  </div>
 </nav>
 <body onload="init()">
 <script src="main.js"></script>
@@ -1522,6 +1504,31 @@
 
 	<div id="map"></div>
   <div></div>
+  <!-- Trigger/Open The Modal -->
+<button id="myBtn"><P>Panier</P></button>
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <ul id="menu">
+
+	  <div id="outer">
+  		<div class="inner">
+		  <button type="submit" onclick="printDiv()"><i ></i> Print</button>
+	  </div>
+	  <div class="inner">
+	  <form action="https://formspree.io/f/mwkzywdl" method="POST"><br>
+                <input type="email" name="_replyto" placeholder="Email"><br>
+                <button type="submit">Devis</button>
+            </form>
+	  </div>
+	  </div>
+    </ul>
+  </div>
+
 <script>
 	/*Ca marche plus*/
 	function recherche() {
@@ -1545,7 +1552,34 @@
         }
     }
 	}
-</script>   
+</script>  
+<script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script> 
 </body>
 
 </html>
