8cf780b639039dca622e9e70b463c358175d40b8
BlascoBasile <blascobasile@gmail.com>
2022-11-17 16:16:53 +0100

    Code bon

--- index.php
+++ index.php
@@ -116,20 +116,6 @@
 
 </body>
 <script>
-    function printDiv() {
-        var divContents = document.getElementById("panier").innerHTML;
-        var a = window.open('', '', 'height=800, width=800');
-        a.document.write('<html>');
-        a.document.write(
-            '<head><link rel="stylesheet" href="./../css/style.css" type="text/css"><link rel="stylesheet" href="./../utils/lib/bootstrap.min.css" /></head>'
-        );
-        a.document.write('<body>');
-        a.document.write(divContents);
-        a.document.write('</body></html>');
-        a.document.close();
-        a.print();
-    }
-
 	function recherche() {
 	var input, filter, found, table, tr, td, i, j;
     input = document.getElementById("myInput");
