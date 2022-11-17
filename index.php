8a5fe8d806be8ea9a2de439cfdca75b1f3ece5fa
BlascoBasile <blascobasile@gmail.com>
2022-11-17 19:01:55 +0100

    tableau

--- index.php
+++ index.php
@@ -1486,12 +1486,12 @@
                         <tr>
                             <th> Formation Continue / initiale : </th>
                             <th> Type d'organisme : </th>
-                            
-                                <th> Intitulé de la formation : </th>
-                                <th> Localisation de la structure (ville) : </th>
-                                <th> Durée de la formation / intervention (en j) : </th>
-                                <th> Modalités d’accès à la formation (présentiel / distanciel / hybride) : </th>
-                                <th></th>
+                            <th> Nom d'organisme : </th>
+                            <th> Intitulé de la formation : </th>
+                            <th> Localisation de la structure (ville) : </th>
+                            <th> Durée de la formation / intervention (en j) : </th>
+                            <th> Modalités d’accès à la formation (présentiel / distanciel / hybride) : </th>
+                            <th></th>
                                 
                             </tr>
                         </thead>
