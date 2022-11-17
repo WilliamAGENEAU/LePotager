663b7f338aa65bdcbde0d648df0e0870d180aa7c
BlascoBasile <blascobasile@gmail.com>
2022-11-17 09:35:58 +0100

    Liste fonctionnelle

--- index.php
+++ index.php
@@ -46,6 +46,61 @@
 	<div id='carte'>
 		<iframe src="https://api.jawg.io/maps/c111bdcc-60ac-4d6b-9713-81ca5d705f0e/b0b3057a-7554-4d39-b93a-af8b7a11184f.html?access-token=mSx8rWaBLl4TKFIAND6r9V8or0NfCPwyPr5QgPVCt4lLcHCAmxzZbawvhGuq9yMD" width="1200px" height="800px"></iframe>
 	</div>
+	<section>
+            <?php
+                $data = file_get_contents("utils/data.geojson");  
+                $data = json_decode($data, true);
+            ?>
+            <div class="tbl-header">
+                <table cellpadding="0" cellspacing="0" border="0">
+                    <thead>
+                        <tr>
+                            <th> Formation Continue / initiale : </th>
+                            <th> Type d'organisme : </th>
+                            
+                            <th> Nom de l'organisme : </th>
+                        
+                            <th> Intitulé de la formation : </th>
+                            <th> Localisation de la structure (ville) : </th>
+                            <th> Durée de la formation / intervention (en j) : </th>
+                            <th> Modalités d’accès à la formation (présentiel / distanciel / hybride) : </th>
+                            
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+            <div class="tbl-content">
+                <table cellpadding="0" cellspacing="0" border="0">
+                    <tbody>
+                            <?php
+                            for($i=0; $i<95;$i++){
+                            ?> 
+                            <tr>
+                                <td><?php echo $data['features'][$i]['properties']["Formation_Continue_initiale"]; ?> </td>
+                                <td><?php echo $data['features'][$i]['properties']["Type_organisme"]; ?> </td>
+                                <td><?php echo $data['features'][$i]['properties']["NOM_de_l'organisme"]; ?> </td>
+                                <td><?php echo $data['features'][$i]['properties']["Intitule_de_la_formation"]; ?> </td>
+                                <td><?php echo $data['features'][$i]['properties']["Localisation_de_la_structure"]; ?> </td>
+                                <td><?php echo $data['features'][$i]['properties']["Duree_de_la_formation"]; ?> </td>
+                                <td><?php echo $data['features'][$i]['properties']["Modalites"]; ?> </td>
+                            </tr>
+                            <?php
+                            }
+                            ?>
+                    </tbody>
+
+                    
+                    <!--<tbody id='data-output'>
+
+
+
+                    </tbody>-->
+
+
+                </table>
+            </div>
+        </section>
+
 </body>
 <script>
     function printDiv() {
--- script.js
+++ script.js
@@ -4,7 +4,7 @@ fetch("utils/data.geojson")
 
                     for(var r=0;r<data.features.length;r++)
                         {
-                        var x=document.getElementById('data-output').insertRow(r);
+                            var x=document.getElementById('data-output').insertRow(r);
                             var y=  x.insertCell(0);
                             y.innerHTML= data.features[r].properties["Formation_Continue_initiale"]
                             var y=  x.insertCell(1);
@@ -19,7 +19,6 @@ fetch("utils/data.geojson")
                             y.innerHTML= data.features[r].properties["Duree_de_la_formation"]
                             var y=  x.insertCell(6);
                             y.innerHTML= data.features[r].properties["Modalites"]
-                            
                         }
                         
                         
